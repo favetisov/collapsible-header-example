a5392c0b785e51888a9a185787a6465ca8e212a5
fedor <f.avetisov@gmail.com>
2021-04-07 20:37:46 +0300

    fixed height adjustments

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -10,7 +10,7 @@ export class AppHome {
       <collapsible-header>
         <collapsible-header-content />
       </collapsible-header>,
-      <main>
+      <main class="wrapper-content">
         <ion-slides>
           <ion-slide>{this.renderContent(1)}</ion-slide>
           <ion-slide>{this.renderContent(2)}</ion-slide>
@@ -24,7 +24,7 @@ export class AppHome {
       <ion-content fullscreen>
         <ion-list>
           {new Array(100).fill(0).map((_, idx) => (
-            <ion-item>
+            <ion-item onClick={() => console.log('clicked')}>
               PAGE #{pageNum} ITEM #{idx}
             </ion-item>
           ))}
--- src/components/collapsible-header/collapsible-header.component.tsx
+++ src/components/collapsible-header/collapsible-header.component.tsx
@@ -1,4 +1,5 @@
-import { Component, h, Element, State, Prop } from '@stencil/core';
+import { Component, h, Host, Element, State, Prop } from '@stencil/core';
+import { readTask, writeTask } from '@stencil/core';
 
 @Component({
   tag: 'collapsible-header',
@@ -13,23 +14,34 @@ export class CollapsibleHeaderComponent {
 
   headerEl: HTMLElement;
   @Element() el: HTMLElement;
+  currentHeight: number;
 
   async componentDidLoad() {
     const pageEl = this.el.closest('ion-app,ion-page,.ion-page,page-inner');
     const contentNodes = pageEl ? pageEl.querySelectorAll('ion-content') : null;
     const scrollEls = [];
+
     for (const node of Array.from(contentNodes)) {
-      const el = await node.getScrollElement();
+      const el = await (node as HTMLIonContentElement).getScrollElement();
       scrollEls.push(el);
+      writeTask(() => {
+        el.style.setProperty('--padding-top', this.maxHeightPx + 'px');
+      });
       el.addEventListener('scroll', e => {
-        const target = e['path'][0];
-        const scrollTop = target.scrollTop;
-        if (scrollTop < this.maxHeightPx - this.minHeightPx) {
-          this.headerEl.style.setProperty('min-height', this.maxHeightPx - scrollTop + 'px');
-          this.scrollElements.forEach(el => {
-            el.scroll(0, scrollTop);
-          });
-        }
+        readTask(() => {
+          const target = e['path'][0];
+          const scrollTop = target.scrollTop;
+          const height = Math.min(Math.max(this.maxHeightPx - scrollTop, this.minHeightPx), this.maxHeightPx);
+          if (height != this.currentHeight) {
+            this.headerEl.style.setProperty('min-height', height + 'px');
+            this.scrollElements
+              .filter(e => e != target)
+              .forEach(el => {
+                el.scroll(0, scrollTop);
+              });
+            this.currentHeight = height;
+          }
+        });
       });
     }
     this.scrollElements = scrollEls;
@@ -37,9 +49,11 @@ export class CollapsibleHeaderComponent {
 
   render() {
     return (
-      <ion-header style={{ 'min-height': this.maxHeightPx + 'px' }} ref={el => (this.headerEl = el)}>
-        <slot></slot>
-      </ion-header>
+      <Host>
+        <ion-header style={{ 'min-height': this.maxHeightPx + 'px' }} ref={el => (this.headerEl = el)}>
+          <slot />
+        </ion-header>
+      </Host>
     );
   }
 }
