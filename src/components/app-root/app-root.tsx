da999acc525f02f1c15273dc8b9f078273281170
fedor <f.avetisov@gmail.com>
2021-04-08 10:26:26 +0300

    removed unused code

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -8,12 +8,30 @@ export class AppRoot {
   render() {
     return (
       <ion-app>
-        <ion-router useHash={false}>
-          <ion-route url="/" component="app-home" />
-          <ion-route url="/profile/:name" component="app-profile" />
-        </ion-router>
-        <ion-nav />
+        <collapsible-header>
+          <collapsible-header-content />
+        </collapsible-header>
+        <main class="wrapper-content">
+          <ion-slides>
+            <ion-slide>{this.renderContent(1)}</ion-slide>
+            <ion-slide>{this.renderContent(2)}</ion-slide>
+          </ion-slides>
+        </main>
       </ion-app>
+    );
+  }
+
+  renderContent(pageNum: number) {
+    return (
+      <ion-content fullscreen>
+        <ion-list>
+          {new Array(100).fill(0).map((_, idx) => (
+            <ion-item onClick={() => console.log('clicked')}>
+              PAGE #{pageNum} ITEM #{idx}
+            </ion-item>
+          ))}
+        </ion-list>
+      </ion-content>
     );
   }
 }
